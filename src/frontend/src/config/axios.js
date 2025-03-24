6966a8e6255d076f87f9f7820ecb7bd89d615941
Maestros <roslyn.bedel@faroty.com>
2025-03-24 07:47:19 +0100

    pret

--- src/frontend/src/config/axios.js
+++ src/frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const apiUrl = "http://localhost:8084/api/v1/snproject";
+const apiUrl = "http://backend-server:8080/api/v1/snproject";
 
 
 axios.defaults.headers.common['Content-Type'] = 'application/json';
